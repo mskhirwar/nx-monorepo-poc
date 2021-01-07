64faa6e445167179c38330ed8168419dc8c6f41f
Manmohan Singh <man.mskhirwar@gmail.com>
2021-01-08 00:50:09 +0530

    api app setup using express

---
+++ apps/api/src/app/todos.ts
@@ -0,0 +1,22 @@
+import {Express} from 'express';
+
+interface Todo {
+    title: string;
+}
+
+const todos: Todo[] = [
+    { title: 'Todo 1'},
+    { title: 'Todo 2'}
+];
+
+export const addTodoRoutes = (app: Express) => {
+    app.get('/api/todos', (req, res) => res.send(todos));
+
+    app.post('/api/addTodo', (req, res) => {
+        const newTodo = {
+            title: `New todo ${Math.floor(Math.random() * 1000)}`,
+        };
+        todos.push(newTodo);
+        res.send(newTodo);
+    });
+};
